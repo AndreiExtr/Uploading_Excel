f6f405cb3a7aa5f9fecbabc232829518f446eaff
Andrey <99189915+AndreiExtr@users.noreply.github.com>
2023-08-06 20:46:02 +0300

    Update Program.cs

--- Programming/Program.cs
+++ Programming/Program.cs
@@ -6,16 +6,7 @@ using Avalonia;
 namespace Programming
 {
     internal class Program
-    {
-        static async Task Main(string[] args)
-        {
-            using var client = new HttpClient();
-            var response = await client.GetStringAsync("https://andreiextr.github.io/Uploading_Excel/");
-
-            Console.WriteLine(response);
-        }
-
-        
+    {   
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
